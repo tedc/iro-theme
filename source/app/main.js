e60703c5877b9611170f1e5635f7f37a12b697e4
tedc <egrandinetti@gmail.com>
2018-03-01 19:39:53 +0100

    timeago

--- source/app/main.js
+++ source/app/main.js
@@ -9,6 +9,7 @@ require('angular-loading-bar');
 require('../../bower_components/ngCart/dist/ngCart')
 require('angular-youtube-embed');
 require('../../bower_components/angular-i18n/angular-locale_it-it');
+require('../../bower_components/angular-timeago/dist/angular-timeago-cor');
 window.controller = new ScrollMagic.Controller()
 var iro = angular.module('iro', ['ngSanitize','angular-loading-bar', 'ui.router', 'yaru22.angular-timeago', 'angularLoad', 'ngAnimate', 'angular-bind-html-compile', 'ngCart', 'ngCookies', 'youtube-embed']);
 const vars = vars;
