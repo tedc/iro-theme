f22accc7bc92aa3041617a92f938473911d5aee7
tedc <egrandinetti@gmail.com>
2018-03-04 11:18:21 +0100

    floor

--- builder/recensioni.php
+++ builder/recensioni.php
@@ -126,8 +126,8 @@ if($reviews->have_posts()) :
 				<span class="section__average"><strong><?php echo $average?></strong> / 5</span>
 				<span class="section__stars">
 					<?php 
-					echo $average . round($average, 0, PHP_ROUND_HALF_DOWN);
-						for($i= 1; $i<= round($average, 0, PHP_ROUND_HALF_DOWN); $i++ ) {
+
+						for($i= 1; $i<= floor($average); $i++ ) {
 							if ($average - $i > 0 && $average - $i < 1) {
 								$is_half = true;
 							} else {
@@ -137,7 +137,7 @@ if($reviews->have_posts()) :
 							$stars = (!$is_half) ? '<i class="icon-stella"></i>' : '<span class="section__starhalf"><i class="icon-stella"></i></span><i class="icon-stella"></i>';
 							echo '<span class="'.$starClass.'">'.$stars.'</span>';
 						}
-						$resto = round((5 - $average), 0, PHP_ROUND_HALF_DOWN);
+						$resto = round((5 - $average), 0, PHP_ROUND_HALF_UP);
 						for($c = 0; $c<$resto; $c++) {
 							echo '<span class="section__star"><i class="icon-stella"></i></span>';
 						}
