37511bde18bec86009794478b2b4ea03d80c28c2
tedc <egrandinetti@gmail.com>
2018-03-05 16:52:51 +0100

    header small

---
+++ template-faq.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Template Name: Faq Template
+ */
+?>
+
+<?php while (have_posts()) : the_post(); ?>
+  <?php get_template_part('templates/page', 'header'); ?>
+  <?php get_template_part( 'builder/init' ); ?>
+<?php endwhile; ?>
