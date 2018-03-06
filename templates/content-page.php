857c0d964e57c3d9dca583614f7b4e429db109dc
tedc <egrandinetti@gmail.com>
2018-03-06 14:52:42 +0100

    is_checkout

--- templates/content-page.php
+++ templates/content-page.php
@@ -1,3 +1,5 @@
+<?php if(!is_checkout()) : ?>
 <div class="page__content page__content--grow-md-bottom page__content--shrink page__content--mw-large">
 	<?php the_content(); ?>
 </div>
+<?php else : the_content(); endif; ?>
