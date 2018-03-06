af223ecc720879cbd9184e9e2c9365c53191ad51
tedc <egrandinetti@gmail.com>
2018-03-06 17:53:25 +0100

    conttatti

--- template-contact.php
+++ template-contact.php
@@ -8,7 +8,7 @@
 	<figure class="contact__cell contact__cell--figure contact__cell--s4">
 		<?php the_post_thumbnail('full', array('class' => 'contact__image')); ?>
 	</figure>
-	<div class="contact__cell contact__cell--grow-lg contact__cell--shrink-fw-right contact__cell--s8">
+	<div class="contact__cell contact__cell--grow-md contact__cell--shrink-fw-right contact__cell--s8">
 		<header  class="contact__header contact__header--shrink-left-only contact__header--grow-md-bottom">
 			<h1 class="contact__title contact__title--medium"><?php the_title(); ?></h1>	
 		</header>
