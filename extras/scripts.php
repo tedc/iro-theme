0726a20268cd6ee9ee2af80dd3a570578463e609
tedc <egrandinetti@gmail.com>
2018-03-06 12:41:09 +0100

    tm

--- extras/scripts.php
+++ extras/scripts.php
@@ -142,6 +142,13 @@ function header_scripts()
   fbq('track', 'PageView');
 </script>
 <!-- End Facebook Pixel Code -->
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-5TPC55T');</script>
+<!-- End Google Tag Manager -->
 	<?php }
 	add_action( 'wp_head','header_scripts');
 
@@ -151,6 +158,10 @@ function header_scripts()
 <noscript><img height="1" width="1" style="display:none"
   src="https://www.facebook.com/tr?id=192305908199271&ev=PageView&noscript=1"
 /></noscript>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5TPC55T"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 	<?php }
 
 	add_action('iro_header', 'body_scripts');
