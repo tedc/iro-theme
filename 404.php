2e73350aeb8f540e2ce548d2bf06dd43645b42b4
tedc <egrandinetti@gmail.com>
2018-03-08 11:53:48 +0100

    grow lg

--- 404.php
+++ 404.php
@@ -1,7 +1,6 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
-<div class="alert alert--shrink alert--mw-large alert--aligncenter">
+<div class="alert alert--grow-lg-bottom alert--shrink alert--mw-large alert--aligncenter">
   <?php _e('Sorry, but the page you were trying to view does not exist.', 'iro'); ?>
 </div>
 
-<?php get_search_form(); ?>
