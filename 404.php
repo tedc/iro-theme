3b33f44446113fd32b0ffc27baee1c4f1adff260
tedc <egrandinetti@gmail.com>
2018-03-08 11:59:13 +0100

    404

--- 404.php
+++ 404.php
@@ -1,7 +1,7 @@
 <?php get_template_part('templates/page', 'header'); ?>
 
 <div class="alert alert--grow-lg-bottom alert--shrink alert--mw-large alert--aligncenter">
-  <?php _e('Spiacenti, la pagina che cerchi non esiste.', 'iro'); ?>
+  <p><?php _e('Spiacenti, la pagina che cerchi non esiste.', 'iro'); ?></p>
   <a href="<?php echo home_url('/'); ?>" ui-sref="app.root()" class="alert__button" style="margin-top:<?php echo (40/16); ?>em"><?php _e('Vai alla home', 'iro'); ?></a>
 </div>
 
