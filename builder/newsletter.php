8af3d813f4e84fbefc23472d5a39681aae6639ac
tedc <egrandinetti@gmail.com>
2018-03-09 14:18:08 +0100

    nl

--- builder/newsletter.php
+++ builder/newsletter.php
@@ -1,2 +1,7 @@
-<form class="newsletter">
+<form class="newsletter newsletter--aligncenter">
+	<fieldset>
+		<legend class="newsletter__title newsletter__title--medium"><?php (get_sub_field('newsletter_title')) ? get_sub_field('newsletter_title') : __('Iscriviti alla newsletter di Iro', 'iro'); ?></legend>
+		<input class="newsletter__input" type="email" ng-model="newsletter_email" placeholder="<?php _e('Il tuo indirizzo email', 'iro'); ?>">
+		<button type="submit" class="newsletter__button"><?php _e('Invia', 'iro'); ?></button>
+	</fieldset>
 </form>
