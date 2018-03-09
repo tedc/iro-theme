b9c7c35bf2d0ceb3640a8c69cc05d75b2c760a12
tedc <egrandinetti@gmail.com>
2018-03-09 14:31:00 +0100

    input

--- builder/newsletter.php
+++ builder/newsletter.php
@@ -1,7 +1,9 @@
 <form class="newsletter newsletter--aligncenter">
 	<fieldset>
 		<legend class="newsletter__title newsletter__title--medium"><?php echo (get_sub_field('newsletter_title')) ? get_sub_field('newsletter_title') : __('Iscriviti alla newsletter di Iro', 'iro'); ?></legend>
-		<input class="newsletter__input" type="email" ng-model="newsletter_email" placeholder="<?php _e('Il tuo indirizzo email', 'iro'); ?>">
-		<button type="submit" class="newsletter__button"><?php _e('Invia', 'iro'); ?></button>
+		<div class="newsletter__row newsletter__row--grid">
+			<input class="newsletter__input" type="email" ng-model="newsletter_email" placeholder="<?php _e('Il tuo indirizzo email', 'iro'); ?>">
+			<button type="submit" class="newsletter__button"><?php _e('Invia', 'iro'); ?></button>
+		</div>
 	</fieldset>
 </form>
