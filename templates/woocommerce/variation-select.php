1b974bf10a3bb08736aee49e44483539fb0cc530
tedc <egrandinetti@gmail.com>
2018-03-12 20:03:22 +0100

    product

--- templates/woocommerce/variation-select.php
+++ templates/woocommerce/variation-select.php
@@ -63,7 +63,7 @@
  
             foreach ( $terms as $term ) { 
                 if ( in_array( $term->slug, $options ) ) {
-                    var_dump(display_price_in_variation_option_name($term->name, $produc));
+                    var_dump(display_price_in_variation_option_name($term->name, $product));
                     // $term_name = '<strong>'.preg_replace('/([0-9]{1,3}([x][0-9]{1,3})([x][0-9]{1,3})?)/', '<span>$1</span>', $term->name) . '</strong><em>'.display_price_in_variation_option_name($term->name, $product).'</em>';
                     // $html .= '<div class="variation__option swiper-slide" ng-click="$event.stopPropagation();singleProductVariation.attribute_' . esc_attr( sanitize_title( $attribute ) ) . '=\''.esc_attr( $term->slug ) .'\'; sizeSelected={name :\''.$term_name.'\', sizes :\''.$term->description.'\'};getVariation();isSelected=false;" ng-class="{\'variation__option--selected\':singleProductVariation.attribute_' . esc_attr( sanitize_title( $attribute ) ) . '==\''.esc_attr( $term->slug ) .'\'}">' . $term_name . '</div>'; 
                 } 
