7f2a994bba0f3ecb00f7aa7ce6d7da4935004faf
tedc <egrandinetti@gmail.com>
2018-03-12 18:31:40 +0100

    price

--- extras/woocommerce.php
+++ extras/woocommerce.php
@@ -33,7 +33,7 @@
     remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_related_products', 20 );
     remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
     remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar', 10 );
-    add_action( 'woocommerce_before_add_to_cart_button', 'woocommerce_template_single_price', 10);
+    //add_action( 'woocommerce_before_add_to_cart_button', 'woocommerce_template_single_price', 10);
     add_filter('woocommerce_show_page_title', '__return_false');
     //remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_title', 5);
     remove_action( 'woocommerce_checkout_order_review', 'woocommerce_checkout_payment', 20 );
