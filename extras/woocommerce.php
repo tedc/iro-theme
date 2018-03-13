53241e7c524a3795169a7f3dea0337d5145439e6
tedc <egrandinetti@gmail.com>
2018-03-13 09:51:58 +0100

    images

--- extras/woocommerce.php
+++ extras/woocommerce.php
@@ -23,10 +23,8 @@
         @remove woocommerce_upsell_display - 15
         @remove woocommerce_output_related_products - 20
      */
-    if(is_mobile()) {
-        remove_action('woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20);
-        add_action('woocommerce_single_product_summary', 'woocommerce_show_product_images', 21);
-    }
+    remove_action('woocommerce_before_single_product_summary', 'woocommerce_show_product_images', 20);
+    add_action('woocommerce_single_product_summary', 'woocommerce_show_product_images', 21);
     remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
     remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
     remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 );
--- woocommerce/content-single-product.php
+++ woocommerce/content-single-product.php
@@ -34,6 +34,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 	 	echo get_the_password_form();
 	 	return;
 	 }
+	 if(has_post_thumbnail()) {
+	 	$img = get_post_thumbnail_id();
+	 	$pt = wp_get_attachment_image_src( $img, 'full', false );
+	 	$pt = ($pt['height'] * 100 / $pt['width'])*50;
+	 	echo '<style>@media screen and (min-width:'.(850/16).'em){#product-'.get_the_ID().':before, #product-'.get_the_ID().'::before {padding-top: '.$pt.'vw;}}'; 
+	 }
 ?>
 <div id="product-<?php the_ID(); ?>" <?php post_class('product--grid product--grow-md-bottom'); ?>>
 
