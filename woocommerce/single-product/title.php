afa6738afdc8cbcc9b09a7ccbba6f98677bb7b0b
tedc <egrandinetti@gmail.com>
2018-03-13 10:15:50 +0100

    stars

--- woocommerce/single-product/title.php
+++ woocommerce/single-product/title.php
@@ -19,5 +19,39 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
-
+$pid = wp_get_post_terms( get_the_ID(), 'prodotto_associato' );
 the_title( '<h1 class="product__title product__title--big">', '</h1>' );
+$args = array(
+	array(
+		'taxonomy' => 'prodotto_associato',
+		'field' => 'term_id',
+		'terms' => array($$pid[0]->term_id)
+	)
+);
+$main_total = count(get_posts(array('post_type' => 'recensioni', 'posts_per_page' => -1, 'tax_query' => $args)));
+$ratings = get_terms(array('taxonomy'=>'rating', 'hide_empty'=>0));
+$totals = array();
+foreach ($ratings as $rate) {
+	$tx = array(
+		'relation' => 'AND',
+		array(
+			'taxonomy' => 'rating',
+			'field' => 'term_id',
+			'terms' => array($rate->term_id)
+		),
+		array(
+			'taxonomy' => 'prodotto_associato',
+			'field' => 'term_id',
+			'terms' => array($$pid[0]->term_id)
+		)
+	);
+	$totals[get_field('rating', 'rating_'.$rate->term_id)] = count(get_posts(array('post_type' => 'recensioni', 'posts_per_page' => -1, 'tax_query' => $tx)));
+}
+$average = 0;
+foreach ($totals as $key => $value) {
+	$average += (intval($key) * $value);
+}
+$average = $average / $main_total;
+$average = round($average, 1);
+?>
+<div class="product__stars"><?php stars($average, 'product'); ?></div>
