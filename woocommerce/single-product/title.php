d486e0b1d16634276a97618507a14c34fe0d77ac
tedc <egrandinetti@gmail.com>
2018-03-13 10:21:14 +0100

    stars

--- woocommerce/single-product/title.php
+++ woocommerce/single-product/title.php
@@ -54,4 +54,4 @@ foreach ($totals as $key => $value) {
 $average = $average / $main_total;
 $average = round($average, 1);
 ?>
-<div class="product__stars"><?php stars($average, 'product'); ?> <?php echo $main_total; ?> <?php _e('recensioni', 'iro'); ?></div>
+<div class="product__stars"><?php stars($average, 'product'); ?> <span class="product__star-label"><?php echo $main_total; ?> <?php _e('recensioni', 'iro'); ?></span></div>
