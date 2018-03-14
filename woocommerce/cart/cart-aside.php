bbade978f5462947e4c03c4878fc06d60f281a92
tedc <egrandinetti@gmail.com>
2018-03-14 11:23:33 +0100

    button

--- woocommerce/cart/cart-aside.php
+++ woocommerce/cart/cart-aside.php
@@ -48,7 +48,7 @@
 					<div class="cart-aside__coupon cart-aside__coupon--shrink cart-aside__coupon--grid-nowrap slide-toggle" ng-class="{'slide-toggle--visible':isCoupon}">
 						<input type="text" name="coupon_code" class="cart-aside__input" ng-model="coupon_code" placeholder="<?php esc_attr_e( 'Inserire codice sconto', 'iro' ); ?>" id="coupon_code" value="" />
 						<input type="hidden" ng-model="coupon_nonce" ng-init="coupon_nonce='<?php echo wp_create_nonce( 'apply-coupon' ); ?>'" />
-						<input type="submit" class="cart-aside__button cart-aside__button--dark cart-aside__button--radius-right" ng-model="apply_coupon" name="apply_coupon" value="<?php esc_attr_e( 'Applica', 'iro' ); ?>" ng-click="ngCart.applyCoupon(coupon_code, coupon_nonce)" />
+						<span class="cart-aside__button cart-aside__button--dark cart-aside__button--radius-right" ng-model="apply_coupon" name="apply_coupon" value="<?php esc_attr_e( 'Applica', 'iro' ); ?>" ng-click="ngCart.applyCoupon(coupon_code, coupon_nonce)"></span>
 					</div>
 					<div class="cart-aside__coupons cart-aside__coupons--shrink" ng-if="ngCart.getExtras().coupons" ng-repeat="coupon in ngCart.getExtras().coupons track by $index" on-finish-render="update_scroller">
 						<div class="cart-aside__coupon">
