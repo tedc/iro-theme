9a275dfc22bdf9dfcf0e6c4a1f438cb3a1370c91
tedc <egrandinetti@gmail.com>
2018-03-14 13:05:15 +0100

    prova

--- extras/woocommerce-api.php
+++ extras/woocommerce-api.php
@@ -401,13 +401,12 @@
 
 		            WC()->session->set( 'chosen_shipping_methods', $chosen_shipping_methods );
 
-		            array_push($coupons, array('free_shipping'=>true));
+		            $coupons['free_shipping']=true;
 		        }
 
 		        do_action( 'woocommerce_applied_coupon', $coupon_code );
-
 		        foreach(WC()->cart->get_applied_coupons() as $code ) {
-		        	array_push($coupons, self::get_coupon($code));
+		        	array_push($coupons['coupons'], self::get_coupon($code));
 		        }
 		        wp_send_json( $coupons );
 	    	} else {
