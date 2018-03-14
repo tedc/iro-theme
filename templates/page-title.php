0b734802238c647ba9bb6e747f7fd3cfa907a22e
tedc <egrandinetti@gmail.com>
2018-03-14 10:14:07 +0100

    is_order_received_page

--- templates/page-title.php
+++ templates/page-title.php
@@ -1,6 +1,7 @@
 <?php
 	use Roots\Sage\Titles; 
 	$title = (get_field('header_alt_title')) ? get_field('header_alt_title') : Titles\title();
+	var_dump(is_order_received_page());
 ?>
 	<h1 class="header__title header__title--big"><?= $title; ?></h1>
 
