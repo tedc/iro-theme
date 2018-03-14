b338f534e84466012ade7c6f0f9177d1fd4bc13a
tedc <egrandinetti@gmail.com>
2018-03-14 20:02:39 +0100

    WC_Product_Variable

--- extras/woocommerce-api.php
+++ extras/woocommerce-api.php
@@ -889,16 +889,17 @@
 	    		foreach(WC()->cart->get_cart() as $cart_item_key => $cart_item) {
 	    			if($cart_item['variation_id']) {
 		    			$_product = new WC_Product_Variable($cart_item['product_id']);
-		    			$variations = $_product->get_available_variations();
-		    			$variation_details = null;
-						if($cart_item['variation_id'] > 0) {
-							foreach($variations as $variation) {
-								if($variation['variation_id'] == $cart_item['variation_id']) {
-									$variation_details = $variation;
-								}
-							}
-		    				$cart_item['variation_details'] = $variation_details;
-		    			}
+		    // 			$variations = $_product->get_available_variations();
+		    // 			$variation_details = null;
+						// if($cart_item['variation_id'] > 0) {
+						// 	foreach($variations as $variation) {
+						// 		if($variation['variation_id'] == $cart_item['variation_id']) {
+						// 			$variation_details = $variation;
+						// 		}
+						// 	}
+		    // 				$cart_item['variation_details'] = $variation_details;
+		    // 			}
+		    			var_dump($product);
 	    			}
 	    			array_push($data['products'], $cart_item);
 	    		}
