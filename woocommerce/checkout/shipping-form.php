88042ef5e22001ea66c68eac68deb98b4e5cd183
tedc <egrandinetti@gmail.com>
2018-03-14 13:55:07 +0100

    prova

--- woocommerce/checkout/shipping-form.php
+++ woocommerce/checkout/shipping-form.php
@@ -3,7 +3,9 @@
 <?php if ( WC()->cart->needs_shipping() && WC()->cart->show_shipping() ) : ?>
 
 	<?php do_action( 'woocommerce_review_order_before_shipping' ); ?>
-		<?php wc_get_template_part('cart/cart', 'shipping' ); ?>
+		<?php //wc_get_template_part('cart/cart', 'shipping' );
+
+		wc_cart_totals_shipping_html() ?>
 		<input type="hidden" name="security" ng-model="checkoutFields.shipping_security" ng-init="checkoutFields.shipping_security='<?php echo wp_create_nonce('update-shipping-method'); ?>'" />
 	<?php do_action( 'woocommerce_review_order_after_shipping' ); ?>
 
