d720374078c21682f07d151bcab300533c232ca3
tedc <egrandinetti@gmail.com>
2018-03-18 18:18:35 +0100

    woocommerce_pay

--- source/app/directives/pay.js
+++ source/app/directives/pay.js
@@ -14,7 +14,6 @@ module.exports = ()=> {
 					_wpnonce : $scope.payFields._wpnonce,
 					key : $scope.payFields.key,
 					payment_method : $scope.payFields.payment_method,
-					woocommerce_pay : $scope.payFields.woocommerce_pay,
 					order_pay : $scope.payFields.order_pay
 				}
 				console.log($scope.payFields);
