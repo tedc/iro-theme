68bf805021718e7ff9e8581439872c4557233a1e
tedc <egrandinetti@gmail.com>
2018-03-18 18:08:36 +0100

    _wpnonce

--- source/app/directives/pay.js
+++ source/app/directives/pay.js
@@ -11,7 +11,7 @@ module.exports = ()=> {
 				$scope.isPaying = true;
 				let url = vars.wc.pay;
 				ecommerce
-					.post(url, $scope.payFields._wpnonce)
+					.post(url, {_wpnonce : $scope.payFields._wpnonce})
 					.then( (res) => {
 						console.log(res);
 						$scope.isPaying = false;
