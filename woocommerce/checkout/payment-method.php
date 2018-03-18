31781d0bf611c42faf791c545442c10f7a80961e
tedc <egrandinetti@gmail.com>
2018-03-18 17:08:00 +0100

    @include grow('md');

--- woocommerce/checkout/payment-method.php
+++ woocommerce/checkout/payment-method.php
@@ -19,10 +19,9 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-var_dump(is_wc_endpoint_url( 'order-pay' ));
 $chosen = ( $gateway->chosen ) ? ' ng-init="checkoutFields.payment_method = \''.esc_attr( $gateway->id ) .'\'; checkoutFields.payment_label = \''. $gateway->get_title() .'\'"' : '';
 ?>
-<li class="payment__method payment__method--grow-md payment__method--grid payment__method--<?php echo $gateway->id; ?>">
+<li class="payment__method payment__method--grow<?php echo (is_wc_endpoint_url( 'order-pay' )) ? '' : '-md'; ?> payment__method--grid payment__method--<?php echo $gateway->id; ?>">
 	<input id="payment_method_<?php echo $gateway->id; ?>" type="radio" class="payment__radio" ng-model="checkoutFields.payment_method" name="payment_method" value="<?php echo esc_attr( $gateway->id ); ?>" <?php checked( $gateway->chosen, true ); ?> data-order_button_text="<?php echo esc_attr( $gateway->order_button_text ); ?>"<?php echo $chosen; ?> />
 
 	<label for="payment_method_<?php echo $gateway->id; ?>" ng-click="checkoutFields.payment_label = '<?php echo $gateway->get_title(); ?>'">
