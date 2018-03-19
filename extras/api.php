8cf66daba73b3b48af005d5e0439639e9fec9ae2
tedc <egrandinetti@gmail.com>
2018-03-19 13:21:49 +0100

    item

--- extras/api.php
+++ extras/api.php
@@ -42,7 +42,7 @@
             // $api->setAccessToken($access_token);
             // $items = $api->Users->Recent($user_id, array('count'=>$count));
             //$cached = get_transient($user_id);
-            return $items;
+            //return $items;
             // if($cached !== false) {
             //     return $cached;
             // } else {
