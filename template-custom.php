4583ce41c810e9e9807a08ca5631f1db26c3f9c2
tedc <egrandinetti@gmail.com>
2018-03-21 12:58:13 +0100

    refix

--- template-custom.php
+++ template-custom.php
@@ -6,10 +6,11 @@
 
 <?php while (have_posts()) : the_post(); 
 	$body_html = get_post_meta($id, 'post_content_html', true);
-if(!empty($body_html)) {
-	echo $body_html;
-} else {
+// if(!empty($body_html)) {
+// 	echo $body_html;
+// } else {
 ?>
   <?php get_template_part('templates/page', 'header'); ?>
   <?php get_template_part( 'builder/init' ); ?>
-<?php } endwhile; ?>
+<?php //} 
+endwhile; ?>
