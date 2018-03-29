c91c8510ba2ef1c2aeb35f8151f4a0c28b1cb3eb
tedc <egrandinetti@gmail.com>
2018-03-29 18:33:37 +0200

    empty

--- woocommerce/checkout/form-checkout.php
+++ woocommerce/checkout/form-checkout.php
@@ -23,7 +23,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 do_action( 'woocommerce_before_checkout_form', $checkout );
 
-if(WC()->cart->is_empty() ) :
+if(!WC()->cart->is_empty() ) :
 
 // If checkout registration is disabled and not logged in, the user cannot checkout
 if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_required() && ! is_user_logged_in() ) {
