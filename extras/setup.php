25a8dc6783e46a10c3653970813635b9aa9e192e
tedc <egrandinetti@gmail.com>
2018-04-04 18:44:49 +0200

    prova

--- extras/setup.php
+++ extras/setup.php
@@ -217,14 +217,17 @@
 	    if( get_query_var( 'downloads' )  ) {
 	    	$base_file = get_field('downloads', 'options');
 	    	$file = $base_file[get_query_var( 'downloads' )];
-	    	if(storeUrlToFilesystem($file, home_url( '/' ))) {
-		        //wp_redirect( home_url( '/' ) );
-
-	    		var_dump(storeUrlToFilesystem($file, home_url( '/' )));
-	    	} else {
-	    		//wp_redirect( home_url( '/' ) );
-	    		var_dump(storeUrlToFilesystem($file, home_url( '/' )));
-	    	}
+	    	if (file_exists($file)) {
+			    header('Content-Description: File Transfer');
+			    header('Content-Type: application/octet-stream');
+			    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+			    header('Expires: 0');
+			    header('Cache-Control: must-revalidate');
+			    header('Pragma: public');
+			    header('Content-Length: ' . filesize($file));
+			    readfile($file);
+			    exit;
+			}
 	    	//exit;
 	    }
 	}
