b6578ae110a44f9419ba0b3714fea23f3f5b5b5b
tedc <egrandinetti@gmail.com>
2018-04-04 19:02:57 +0200

    prova

--- extras/setup.php
+++ extras/setup.php
@@ -227,10 +227,10 @@
 	    		curl_close($ch);
 			    header('Content-Description: File Transfer');
 			    header('Content-Type: application/octet-stream');
-			    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+			    header('Content-Disposition: attachment; filename='.basename($file));
 			    header('Expires: 0');
-			    header('Cache-Control: must-revalidate');
-			    header('Pragma: public');
+			    //header('Cache-Control: must-revalidate');
+			    header('Pragma: no-cache');
 			    header('Content-Length: ' . $size);
 			    //header('Set-Cookie: fileLoading=true');
 			    //header( 'refresh:5;url='.home_url('/'));
