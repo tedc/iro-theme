0b5d09d72971c267261fa1543f51faa9df1d98ec
tedc <egrandinetti@gmail.com>
2018-04-04 19:05:37 +0200

    mah

--- extras/setup.php
+++ extras/setup.php
@@ -218,20 +218,20 @@
 	    	$base_file = get_field('downloads', 'options');
 	    	$file = $base_file[get_query_var( 'downloads' )];
 	    	if ($file) {
-	    		$ch = curl_init($file);
-	    		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-	    		curl_setopt($ch, CURLOPT_HEADER, TRUE);
-	    		curl_setopt($ch, CURLOPT_NOBODY, TRUE);
-	    		$data = curl_exec($ch);
-	    		$size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
-	    		curl_close($ch);
+	    		// $ch = curl_init($file);
+	    		// curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+	    		// curl_setopt($ch, CURLOPT_HEADER, TRUE);
+	    		// curl_setopt($ch, CURLOPT_NOBODY, TRUE);
+	    		// $data = curl_exec($ch);
+	    		// $size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+	    		// curl_close($ch);
 			    header('Content-Description: File Transfer');
 			    header('Content-Type: application/octet-stream');
 			    header('Content-Disposition: attachment; filename='.basename($file));
 			    header('Expires: 0');
 			    //header('Cache-Control: must-revalidate');
 			    header('Pragma: no-cache');
-			    header('Content-Length: ' . $size);
+			    //header('Content-Length: ' . $size);
 			    //header('Set-Cookie: fileLoading=true');
 			    //header( 'refresh:5;url='.home_url('/'));
 			    echo file_get_contents($file);
