1d762b5bfaf8ce0b7d6b1cd8c15f29eda8dd5de6
tedc <egrandinetti@gmail.com>
2018-04-05 09:02:06 +0200

    prova

--- extras/setup.php
+++ extras/setup.php
@@ -246,7 +246,7 @@
 	}
 
 	function my_dlm_download_headers($headers) {
-		$headers['refresh'] = '5';
+		//$headers['refresh'] = '5';
 		return $headers;
 	}
 	add_filters('dlm_download_headers', 'my_dlm_download_headers', 10, 1);
