76e73322146cd3789bc27b9706e8e114ccbf3080
tedc <egrandinetti@gmail.com>
2018-04-13 18:00:20 +0200

    df

--- templates/promo.php
+++ templates/promo.php
@@ -5,7 +5,7 @@
 	);
 	$promo = new WP_Query($args);
 	if($promo->have_posts()) : ?>
-<div class="promo promo--grid" ng-promo>
+<div class="promo promo--grid-nowrap" ng-promo>
 	<?php while($promo->have_posts()) : $promo->the_post(); ?>
 	<div class="promo__pre promo__pre--shrink-fw-left"><span><?php the_title(); ?></span></div>
 	<div class="promo__content promo__content--shrink-fw-right">
