0c0485adf144b8def3c54874fbad824151a6cd25
tedc <egrandinetti@gmail.com>
2018-04-13 16:55:52 +0200

    php fix

--- templates/promo.php
+++ templates/promo.php
@@ -6,9 +6,11 @@
 	$promo = new WP_Query($args);
 	if($promo->have_posts()) : ?>
 <div class="promo promo--grid" ng-promo>
+	<?php while($promo->have_posts()) : $promo->the_post(); ?>
 	<div class="promo__pre promo__pre--shrink-fw-left"><?php the_title(); ?></div>
 	<div class="promo__content promo__content--shrink-fw-right">
 		<?php the_content(); ?>
 	</div>
+<?php endwhile; wp_reset_query(); wp_reset_postdata(); ?>
 </div>
 <?php endif; ?>
