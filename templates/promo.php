b551703497bd04e577e4b88a296163374fc55464
tedc <egrandinetti@gmail.com>
2018-04-16 12:40:09 +0200

    promo

--- templates/promo.php
+++ templates/promo.php
@@ -5,7 +5,8 @@
 	);
 	$promo = new WP_Query($args);
 	if($promo->have_posts()) : ?>
-<div class="promo promo--grid-nowrap" ng-promo>
+<div class="promo" ng-promo>
+	<div class="promo__wrapper promo__wrapper--grid-nowrap">
 	<?php while($promo->have_posts()) : $promo->the_post(); ?>
 	<div class="promo__pre promo__pre--shrink-fw-left"><span><?php the_title(); ?></span></div>
 	<div class="promo__content promo__content--shrink-fw-right">
@@ -17,5 +18,6 @@
 		 ?>
 	</div>
 <?php endwhile; wp_reset_query(); wp_reset_postdata(); ?>
+</div>
 </div>
 <?php endif; ?>
