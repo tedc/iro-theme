2feba2d0e9cb6b35b2826866f3816246b0e7fbcf
tedc <egrandinetti@gmail.com>
2018-04-24 18:33:25 +0200

    console.log(cookie);

--- source/app/directives/index.js
+++ source/app/directives/index.js
@@ -338,7 +338,9 @@ iro
 			link : (scope, element, attr)=> {
 				$rootScope.isCountDown = false;
 				let cookie = $cookies.get(attr.cookieName);
+				console.log(cookie);
 				if(!cookie && typeof cookie == 'undefined') {
+					console.log(cookie);
 					let date = new Date();
         			date.setTime(date.getTime()+(24*60*60*1000))
 					$cookies.put(attr.cookieName, true, {
