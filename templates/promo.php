db314e58e6a5047cfe424a76bae41ffef8dcf0c6
tedc <egrandinetti@gmail.com>
2018-04-24 16:23:56 +0200

    popup

--- templates/promo.php
+++ templates/promo.php
@@ -26,7 +26,7 @@
 				<div class="popup__figure popup__figure--cell-s6">
 					<?php the_post_thumbnail('full'); ?>
 				</div>
-				<div class="popup__content popup__content--grid popup__content--cell-s6">
+				<div class="popup__content popup__content--shrink popup__content--grid popup__content--cell-s6">
 					<h3 class="popup__title"><?php the_field('popup_title'); ?></h3>
 					<div class="popup__countdown popup__countdown--grid popup__countdown--grow-top" ng-countdown="<?php the_field('popup_date'); ?>">
 						<div class="popup__time">
@@ -45,14 +45,14 @@
 							<strong>{{s}}</strong><br />
 							<?php _e('Secondi', 'iro'); ?>
 						</div>
-					</div>
+					</div>	
+					<?php if(get_field('popup_text')): ?>
+					<div class="popup__text popup__text--grow-top"><?php the_field('popup_text'); ?></div>
+					<?php endif; ?>
+					<?php if(get_field('popup_link')): ?>
+					<div class="popup__link popup__link--grow-top"><a class="popup__button" hre="<?php the_field('popup_link'); ?>"><?php _e('Acquista IRO', 'iro'); ?></a></div>
+					<?php endif; ?>
 				</div>
-				<?php if(get_field('popup_text')): ?>
-				<div class="popup__text popup__text--grow-top"><?php the_field('popup_text'); ?></div>
-				<?php endif; ?>
-				<?php if(get_field('popup_link')): ?>
-				<div class="popup__link popup__link--grow-top"><a class="popup__button" hre="<?php the_field('popup_link'); ?>"><?php _e('Acquista IRO', 'iro'); ?></a></div>
-				<?php endif; ?>
 			</div>
 		</div>
 	</div>
