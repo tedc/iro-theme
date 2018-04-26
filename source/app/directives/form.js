4a10f91a1c9f6b14d9cc6ac291951438f49141e5
tedc <egrandinetti@gmail.com>
2018-04-26 15:56:00 +0200

    form

--- source/app/directives/form.js
+++ source/app/directives/form.js
@@ -8,44 +8,49 @@ module.exports = ()=> {
 		 	$scope.submit = (isValid)=> {
 		 		if($scope.isSubmitted) return;
 		 		$scope.isSubmitted = true;
-            	let url = (is_size_form) ? vars.wc.form : vars.wc.size_form;
-            	let frmdata = $scope.formData;
-            	let config = {
-            		headers : { 
-            			"Content-type" : "application/x-www-form-urlencoded; charset=utf-8"
-            		}
-            	};
-            	$scope.formData = {}
-         	  	$scope.contactForm.$setUntouched();
-          		$scope.contactForm.$setPristine();
-          		if(isValid) {
-          			$http
-          				.post(url, $httpParamSerializerJQLike(frmdata), config)
-          				.then( (res)=> {		
-          					if(!res) return;
-          					if($window.dataLayer) $window.dataLayer.push({event: 'formSubmissionSuccess'});
-          					$scope.isContactSent = true;
-          					$scope.alert = res.data.formMsg;
-                    if(is_size_form) {
-                      $scope.formData = {
-                        _bcc : frmdata._bcc,
-                        _send_to : frmdata._send_to
-                      }; 
-                    } else {
-                       $scope.formData = {
-                        _iro_form_nonce : res.data.new_nonce,
-                        _bcc : frmdata._bcc,
-                        _send_to : frmdata._send_to
-                      }; 
-                    }
-          					
-          					$timeout(()=> {
-                                $scope.isSubmitted = false;
-                                $scope.isContactSent = false;
-          					}, 5000);
-          				});
-          		}
-            }
+      	let url = (is_size_form) ? vars.wc.form : vars.wc.size_form;
+      	let frmdata = $scope.formData;
+      	let config = {
+      		headers : { 
+      			"Content-type" : "application/x-www-form-urlencoded; charset=utf-8"
+      		}
+      	};
+      	$scope.formData = {}
+        if(is_size_form) {
+          $scope.sizeForm.$setUntouched();
+          $scope.sizeForm.$setPristine();
+        } else {
+          $scope.contactForm.$setUntouched();
+          $scope.contactForm.$setPristine();
+        }
+    		if(isValid) {
+    			$http
+    				.post(url, $httpParamSerializerJQLike(frmdata), config)
+    				.then( (res)=> {		
+    					if(!res) return;
+    					if($window.dataLayer) $window.dataLayer.push({event: 'formSubmissionSuccess'});
+    					$scope.isContactSent = true;
+    					$scope.alert = res.data.formMsg;
+              if(is_size_form) {
+                $scope.formData = {
+                  _bcc : frmdata._bcc,
+                  _send_to : frmdata._send_to
+                }; 
+              } else {
+                 $scope.formData = {
+                  _iro_form_nonce : res.data.new_nonce,
+                  _bcc : frmdata._bcc,
+                  _send_to : frmdata._send_to
+                }; 
+              }
+    					
+    					$timeout(()=> {
+                          $scope.isSubmitted = false;
+                          $scope.isContactSent = false;
+    					}, 5000);
+    				});
+    		}
+      }
 		 }]
 	}
 }
