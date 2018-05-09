401e3a7f39b19942352b9af2bc3a1ad8b9e1a7f7
tedc <egrandinetti@gmail.com>
2018-05-09 15:39:30 +0200

    virgola

--- templates/woocommerce/variation-select.php
+++ templates/woocommerce/variation-select.php
@@ -21,7 +21,7 @@
             $count_opt = 0;
             foreach ( $terms as $term ) { 
                 if ( in_array( $term->slug, $options ) ) {
-                    $term_name = '<strong>'.preg_replace('/([0-9]{1,3}([x][0-9]{1,3})([x][0-9]{1,3}[,][0-9]{1,2})?)/', '<span>$1</span>', $term->name) . '</strong>';
+                    $term_name = '<strong>'.preg_replace('/([0-9]{1,3}([x][0-9]{1,3})([x][0-9]{1,3})?([,][0-9]{1,2})?)/', '<span>$1</span>', $term->name) . '</strong>';
                     
                      $ngselected = $args['selected'] == $term->slug ? ' ng-init=";variationPriceIndex='.$count_opt.';singleProductVariation.attribute_' . esc_attr( sanitize_title( $attribute ) ) . '=\''.$args['selected'].'\'; sizeSelected={name :\''.$term_name.'\', sizes :\''.$term->description.'\'}"' : '';
                     
@@ -67,7 +67,7 @@
                     $_prices = htmlspecialchars( wp_json_encode( display_price_in_variation_option_name($term->name, $product) ) );
                     $_is_custom = (get_field('is_custom_size', $term)) ? 'true' : 'false';
                     //$_prices = str_replace(array('[', ']'), '', $_prices);
-                    $term_name = '<strong>'.preg_replace('/([0-9]{1,3}([x][0-9]{1,3})([x][0-9]{1,3}[,]{1,2})?)/', '<span>$1</span>', $term->name) . '</strong>';
+                    $term_name = '<strong>'.preg_replace('/([0-9]{1,3}([x][0-9]{1,3})([x][0-9]{1,3})?([,][0-9]{1,2})?)/', '<span>$1</span>', $term->name) . '</strong>';
                     $html .= '<div class="variation__option swiper-slide" ng-click="$event.stopPropagation();singleProductVariation.attribute_' . esc_attr( sanitize_title( $attribute ) ) . '=\''.esc_attr( $term->slug ) .'\'; sizeSelected={name :\''.$term_name.'\', sizes :\''.$term->description.'\'};getVariation();isSelected=false;variationPriceIndex='.$count_select.';isCustomSize='.$_is_custom.';" ng-class="{\'variation__option--selected\':singleProductVariation.attribute_' . esc_attr( sanitize_title( $attribute ) ) . '==\''.esc_attr( $term->slug ) .'\'}" ng-init="variationPrices['.$count_select.']='.$_prices.'">' . $term_name . '<em ng-bind-html="variationPrice('.$count_select.')"></em></div>'; 
                     $count_select++;
                 } 
