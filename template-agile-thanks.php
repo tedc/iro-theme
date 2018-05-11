8ab29c835a9fd4a9d1e38c82ce2fdc9e22b275fa
tedc <egrandinetti@gmail.com>
2018-05-11 11:31:35 +0200

    title

--- template-agile-thanks.php
+++ template-agile-thanks.php
@@ -7,7 +7,11 @@ acf_set_language_to_default();
 	acf_unset_language_to_default();
 ?>
 <?php while (have_posts()) : the_post(); ?>
-	<div class="header header--page header--shrink-fw"><h1 class="header__title"><?php the_title(); ?></h1></div>
+	<div class="header header--page header--shrink-fw">
+		<div class="header__container header__container--grow-md">
+			<?php get_template_part( 'templates/page', 'title'); ?>
+		</div>
+	</div>
 	<div class="alert alert--grow-lg-bottom alert--shrink alert--mw-large alert--aligncenter">
 	<?php the_content(); ?>
   	<a href="<?php echo get_permalink($materasso); ?>" class="page__button"><?php _e('Continua lo shopping'); ?></a>
