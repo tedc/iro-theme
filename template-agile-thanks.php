f1a0c65389a48f3c11960941a489d4fee6d2ff5a
tedc <egrandinetti@gmail.com>
2018-05-11 13:21:03 +0200

    prova

--- template-agile-thanks.php
+++ template-agile-thanks.php
@@ -6,7 +6,7 @@
 	$materasso = get_field('materasso', 'options');
 	acf_unset_language_to_default();
 	$promo_id = intval(get_query_var('promo_id'));
-	$date = get_field('popup_expire', $promo_id, false, false);
+	$date = get_field('popup_expire', $promo_id);
 	echo $date;
 ?>
 <?php while (have_posts()) : the_post(); ?>
