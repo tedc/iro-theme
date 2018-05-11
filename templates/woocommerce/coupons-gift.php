d186dc0294c1951df008054963867118fcce0d1b
tedc <egrandinetti@gmail.com>
2018-05-11 17:16:33 +0200

    send coupons

--- templates/woocommerce/coupons-gift.php
+++ templates/woocommerce/coupons-gift.php
@@ -1,4 +1,4 @@
-<div class="free-gifts">
+<div class="free-gifts" ng-class="{'free-gifts--disabled':ngCart.isGiftDisabled(<?php echo $free_gift_max; ?>)}">
 	<h4 class="free-gifts__title"><?php echo ($free_gift_title) ? $free_gift_title : __('Seleziona uno dei prodotti in omaggio con il tuo coupon'); ?></h4>
 <?php
 	$args = array(
@@ -8,12 +8,12 @@
 	);
 	$gifts = new WP_Query($args);
 	while ($gifts->have_posts()) : $gifts->the_post(); ?>
-		<div class="free-gifts__row" ng-class="{'free-gifts__row--disabled':ngCart.isGiftDisabled(<?php the_ID(); ?>, <?php echo $free_gift_max; ?>)}">
+		<div class="free-gifts__row">
 			<div class="free-gifts__content free-gifts__content--grid" for="free-gift_<?php the_ID(); ?>">
 				<?php the_post_thumbnail( 'post-thumbnail' ); ?>
 				<a class="free-gifts__link" href="<?php the_permalink(); ?>" target="_blank"><?php the_title(); ?></a>
-				<div class="free-gift__select" ng-click="isRow[<?php the_ID(); ?>]=!isRow[<?php the_ID(); ?>]" click-outside="isRow[<?php the_ID(); ?>]=false">
-					<span class="free-figts__value"><span ng-bind-html="(giftRow[<?php the_ID(); ?>]) ? giftRow[<?php the_ID(); ?>] : '<?php _e('Seleziona', 'iro'); ?>'"></span><i class="icon-arrow-down"></i></span>
+				<div class="free-gifts__select" click-outside="isRow[<?php the_ID(); ?>]=false">
+					<span class="free-gifts__value" ng-click="isRow[<?php the_ID(); ?>]=true;"><span ng-bind-html="(giftRow[<?php the_ID(); ?>]) ? giftRow[<?php the_ID(); ?>] : '<?php _e('Seleziona', 'iro'); ?>'"></span><i class="icon-arrow-down"></i></span>
 					<ul class="free-gifts__options" ng-class="{'free-gifts__options--visible':isRow[<?php the_ID(); ?>]}">
 						<?php for($i=0; $i<=$free_gift_max; $i++) : 
 						?>
@@ -21,9 +21,10 @@
 							<?php echo ($i==0) ? __('Seleziona', 'iro') : $i; ?>
 						</li>
 						<?php endfor; ?>
+					</ul>
 				</div>
-
 			</div>
 		</div>
 <?php endwhile; wp_reset_postdata(); wp_reset_query(); ?>
+	<span class="free-gift__button" ng-click="ngCart.resetGift();"><?php _e('Resetta omaggi', 'iro'); ?><i class="icon-close"></i></span>
 </div>
