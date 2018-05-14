bfb934706af39d8266e0fe33cab4099a4dee6152
tedc <egrandinetti@gmail.com>
2018-05-14 14:41:58 +0200

    $order->get_id()

--- woocommerce/order/order-details.php
+++ woocommerce/order/order-details.php
@@ -81,14 +81,14 @@ if ( $show_customer_details ) {
 					<td><?php echo wptexturize( $order->get_customer_note() ); ?></td>
 				</tr>
 			<?php endif;
-				if(get_post_meta('_free_gift_total', $order->gt_id(), true)) : 
+				if(get_post_meta('_free_gift_total', $order->get_id(), true)) : 
 			 ?>
 			 <div class="order-details__row">
 			 	<strong><?php _e('Prodotti omaggio', 'iro'); ?></strong>
 			 <?php		
-					for($i = 0; $i < get_post_meta($order->id, '_free_gift_total', true); $i++) {
+					for($i = 0; $i < get_post_meta($order->get_id(), '_free_gift_total', true); $i++) {
 			 ?>
-			 <strong><?php echo get_the_title(get_post_meta( $order_id, '_free_gift_product_id_'.$i, true)); ?> x<?php echo get_post_meta( $order_id, '_free_gift_product_qty_'.$i, true); ?></strong>
+			 <strong><?php echo get_the_title(get_post_meta($order->get_id(), '_free_gift_product_id_'.$i, true)); ?> x<?php echo get_post_meta( $order->get_id(), '_free_gift_product_qty_'.$i, true); ?></strong>
 
 			<?php } ?>
 		</div>
