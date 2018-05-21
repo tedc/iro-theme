6029a5f82e17b4bd64b23ef3a8383a5fb355aa6f
tedc <egrandinetti@gmail.com>
2018-05-21 18:07:36 +0200

    iro

--- woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
+++ woocommerce/single-product/add-to-cart/variation-add-to-cart-button.php
@@ -12,8 +12,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 global $product;
-$defautls = iconic_get_default_attributes($product);
-$variation_id = iconic_find_matching_product_variation($product, $defautls);
+$defautls = iro_get_default_attributes($product);
+$variation_id = iro_find_matching_product_variation($product, $defautls);
 ?>
 <input type="hidden" ng-init="product.original_id=<?php echo $product->get_id(); ?>" ng-model="product.origina_id" />
 <ngcart-addtocart id="{{(product.product_id) ? product.product_id : <?php echo ($variation_id > 0) ? $variation_id : $product->get_id(); ?>}}" price="<?php echo $product->get_price(); ?>" name="<?php echo $product->get_title(); ?>" quantity="<?php echo $product->get_min_purchase_quantity(); ?>"<?php if($product->get_max_purchase_quantity() > 0) : ?> quantity-max="<?php echo $product->get_max_purchase_quantity(); ?>"<?php endif; ?> template-url="addtocart.html" data="{href : '<?php echo $product->get_slug(); ?>'}">
