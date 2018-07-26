9fe2fdec32a9d933c21dd5c599947c27a3b7e63d
tedc <egrandinetti@gmail.com>
2018-07-26 16:07:02 +0200

    title

--- templates/page-title.php
+++ templates/page-title.php
@@ -10,10 +10,10 @@
 	<figure class="header__success">
 		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/superhero-ordine-effettuato.gif" />
 	</figure>
-	<?php endif; ?>
+	<?php endif; if(!empty($title)) :?>
 	<h1 class="header__title header__title--big<?php echo (is_order_received_page()) ? ' header__title--aligncenter' : ''; ?>"><?= $title; ?></h1>
 
-	<?php while(have_rows('header_button')) : the_row();
+	<?php endif; while(have_rows('header_button')) : the_row();
 		$button_class = 'header__button';
 		$button_class .= get_sub_field('header_button_color') ? ' header__button--'.get_sub_field('header_button_color') : '';
 		if(get_sub_field('header_button_link')):
